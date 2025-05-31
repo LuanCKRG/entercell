f3fe1c928991c9288d919030f5c6db3fb7e0e38e
LuanCKRG <luancamposck@gmail.com>
2025-05-31 18:33:19 -0300

    feat(hero-section): improve responsiveness

--- src/components/sections/hero-section.tsx
+++ src/components/sections/hero-section.tsx
@@ -4,17 +4,17 @@ import { ArrowRight } from "lucide-react"
 import { Button } from "@/components/ui/button"
 
 export const HeroSection = () => (
-	<section className="p-8 grid grid-cols-2 py-20">
-		<div className="flex flex-col justify-between">
+	<section className="md:p-8 flex md:grid md:grid-cols-2">
+		<div className="flex flex-col justify-between gap-y-6">
 			<div>
-				<Button variant="outline" className="rounded-full border-border gap-x-1 w-fit group px-3 py-1 hover:border-[#fcd20b]" asChild>
+				<Button variant="outline" className="flex mx-auto md:mx-0 rounded-full border-border gap-x-1 w-fit group px-3 py-1 hover:border-[#fcd20b]" asChild>
 					<a href="/">
 						<span className="font-mono text-xs font-medium group-hover:text-[#fcd20b]">Peça seu orçamento grátis</span>
 						<ArrowRight className="size-4 transition group-hover:translate-x-1 group-hover:text-[#fcd20b]" />
 					</a>
 				</Button>
 
-				<h2 className="max-w-[580px] text-zinc-300 leading-tight text-[24px] md:text-[36px] font-medium">
+				<h2 className="mt-8 max-w-[580px] text-zinc-300 leading-tight text-[24px] md:text-[36px] font-medium">
 					Conserto de celular <span className="underline font-bold text-white decoration-[#fcd20b]">rápido</span>,{" "}
 					<span className="underline font-bold text-white decoration-[#fcd20b]">garantido</span> e com preço{" "}
 					<span className="underline font-bold text-white decoration-[#fcd20b]">justo</span>!
@@ -29,8 +29,8 @@ export const HeroSection = () => (
 			</Button>
 		</div>
 
-		<div className="flex justify-center items-center">
-			<Image className="w-52" src="/logo_cell.svg" alt="" width={200} height={400} />
+		<div className="hidden md:flex justify-center items-center">
+			<Image className="w-20 md:w-52" src="/logo_cell.svg" alt="" width={200} height={400} />
 		</div>
 	</section>
 )
