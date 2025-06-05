e9159481f067f37aab18ea7c087eb6445aa1c514
LuanCKRG <luancamposck@gmail.com>
2025-06-05 19:57:54 -0300

    feat(layout): add WhatsApp link with icon to the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next"
+import Image from "next/image"
 import { Geist, Geist_Mono } from "next/font/google"
 
 import "./globals.css"
@@ -23,6 +24,15 @@ const RootLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
 		<html lang="pt-BR">
 			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
 				<main className="bg-gradient-to-br from-slate-50 to-blue-50">{children}</main>
+
+				<a
+					href="https://wa.me/5511947350134"
+					target="_blank"
+					rel="noopener noreferrer"
+					className="p-4 rounded-full fixed bottom-6 right-6 bg-[#25d366] hover:bg-[#0a8c7e] transition-colors duration-300"
+				>
+					<Image src="/icons/whatsapp-icon.svg" alt="whatsapp" width={100} height={100} className="size-8" />
+				</a>
 			</body>
 		</html>
 	)
